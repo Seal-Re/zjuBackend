5c445727c445554e03ed0c815f0533d8cf4970cf
Seal-Re <3291351273@qq.com>
2025-12-19 15:26:50 +0800

    Revert "Revert "测试用例全量更新""
    This reverts commit 500a6abfdad7097244cb979d90ad32094e175ba6.

---
+++ fastop/fastop-service/src/main/java/com/hengtiansoft/fastop/service/designer/service/TestExampleService.java
@@ -0,0 +1,9 @@
+package com.hengtiansoft.fastop.service.designer.service;
+
+import com.hengtiansoft.fastop.base.common.entity.Response.Response;
+
+public interface TestExampleService {
+
+    Response UpdateAll();
+
+}
